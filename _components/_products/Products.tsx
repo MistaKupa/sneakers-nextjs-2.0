6b3b30f5a8250abb1f5149427f7d8cd8920e1ff5
Jakub Kubis <jakub.kubis11@gmail.com>
2026-03-16 20:18:11 +0100

    changed background color

--- _components/_products/Products.tsx
+++ _components/_products/Products.tsx
@@ -54,7 +54,7 @@ export default function Products({
       </div>
 
       {/* Product grid */}
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-4 ">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
